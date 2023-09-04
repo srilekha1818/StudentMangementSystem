136554e57babc0ebbbd08e9fb5a9ff5cd128c4d3
srilekha1818 <srilekhakavuri18@gmail.com>
2023-09-04 21:26:16 +0530

    commit

--- StudentManagementSystem/src/main/java/com/student/manage/service/StudentService.java
+++ StudentManagementSystem/src/main/java/com/student/manage/service/StudentService.java
@@ -3,14 +3,33 @@ package com.student.manage.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.apache.tomcat.util.http.parser.MediaTypeCache;
+
 import com.student.manage.entity.Student;
 
+import jakarta.websocket.server.PathParam;
+
+import javax.ws.rs.core.MediaType;
+//@Consumes("application/xml,application/json")
+//@Produces("application/xml,application/json")
+@Consumes(MediaType.TEXT_HTML)
+@Produces(MediaType.TEXT_HTML)
+@Path("/studentservice")
 public interface StudentService  {
 List<Student> getAllStudents();
 	
 	Student saveStudent(Student student);
-	
-	Student getStudentById(Long id);
+	/*@Path("/patients/{id}")
+	@GET
+	Patient getPatient(@PathParam("id") Long id);	*/
+	@Path("/edit/{id}")
+	@GET
+	Student getStudentById(@PathParam("id")Long id);
 	
 	Student updateStudent(Student student);
 	
